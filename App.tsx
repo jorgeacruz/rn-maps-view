31c89a56b1d396a865ade1c99b6ca092ca2ebb9a
Jorge Cruz <j.alexcruz@gmail.com>
2025-07-10 19:20:48 -0300

    Add some styles

--- App.tsx
+++ App.tsx
@@ -5,31 +5,29 @@ import MapView, { Marker } from 'react-native-maps';
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.viewTitle}>
+        <Text style={styles.title}> 
+          Cidade Maravilhosa
+        </Text>
+      </View>
       
       <MapView style={styles.maps}
       // Map configuration
        initialRegion={{
           latitude: -22.9068,
           longitude: -43.1729,
-          latitudeDelta: 0.100,
-          longitudeDelta: 0.100,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
         }}
         // Map features
         mapType="terrain"
-        // Map controls
-        showsMyLocationButton={true}
-        showsCompass={true}
-        rotateEnabled={true}
-        scrollEnabled={true}
-        zoomEnabled={true}
-        pitchEnabled={true}
-        showsUserLocation={true}
-        showsScale={true}
-        showsTraffic={false}
-        showsIndoors={true}
-        showsBuildings={true}
-        showsPointsOfInterest={true}>
-
+       
+        >
+          <Marker
+            coordinate={{ latitude: -22.9068, longitude: -43.1729 }}
+            title="Rio de Janeiro"
+            description="Cidade Maravilhosa"
+          />
       </MapView>
       <StatusBar style="light" />
     </View>
@@ -47,5 +45,16 @@ const styles = StyleSheet.create({
   maps:{
     width: '100%',
     height: '100%',
+  },
+  title:{
+    color: '#000', fontWeight:'bold' , fontSize: 30, textAlign: 'center', marginTop: 20
+  },
+  viewTitle:{
+    position: 'absolute',
+        top: 50,
+        left: 0,
+        right: 0,
+        alignItems: 'center',
+        zIndex: 1,
   }
 });
