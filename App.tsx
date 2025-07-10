bc81c6e9fc46a04a54826dcb29f0c3cd017d54d1
Jorge Cruz <j.alexcruz@gmail.com>
2025-07-10 18:34:22 -0300

    First Commit + React Maps View

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.maps}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        showsCompass={true}
+        showsScale={true}
+        showsTraffic={true}
+        showsIndoors={true}
+        showsBuildings={true}
+        showsPointsOfInterest={true}>
+
+      </MapView>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +30,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  maps:{
+    width: '100%',
+    height: '100%',
+  }
 });
